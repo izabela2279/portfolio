1611f07be4e708c9e3ace8de796e502137af876c
Izabela <izabelaszeremeta@gmail.com>
2023-01-31 20:00:29 -0600

    add data

---
+++ src/data/projects.jsx
@@ -0,0 +1,18 @@
+export const projects = [
+  {
+    id: 0,
+    title: "Quiz Game",
+    description: "Games",
+    image: "https://espresso-quiz.netlify.app/",
+    repositoryLink: "https://github.com/izabela2279/quiz-game.git",
+    deploymentLink: "https://espresso-quiz.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "Score Predictor",
+    description: "Games",
+    image: "https://score-predictor.fly.dev",
+    repositoryLink: "https://github.com/izabela2279/score-predictor.git",
+    deploymentLink: "https://score-predictor.fly.dev",
+  }
+]
