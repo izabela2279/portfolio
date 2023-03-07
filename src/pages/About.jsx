9ba4676c5b1aafbf90e412fb9f5efba55fa1ffac
Izabela <izabelaszeremeta@gmail.com>
2023-03-07 10:42:26 -0600

    update favicon

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ function About() {
     <>
       <img src='./profile.jpg' alt="profile" />
       <h2>
-        Software Engineering student enrolled in coding bootcamp with experience creating applications using JavaScript. With 6 years of experience in customer service in financial and sales environments. Exceptionally detail oriented and enthusiastic about learning new technologies.
+      Software Engineer with 6 years of experience in customer service in financial and sales environments. Adept at creating applications using JavaScript. Exceptionally detail oriented and enthusiastic about learning new technologies.
       </h2>
     </>  
   )
